6496af9df6b76e58c89649556d874bd192803f99
Ahmed-Sherif-ASA <ahmedsherifakl.asa@gmail.com>
2020-07-15 02:03:32 +0200

    Image Manipulator

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -10,7 +10,10 @@ import { View, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { Input, CheckBox, Button, Icon } from 'react-native-elements';
 import * as SecureStore from 'expo-secure-store';
 import * as Permissions from 'expo-permissions';
-import * as ImagePicker from 'expo-image-picker'
+import * as ImagePicker from 'expo-image-picker';
+import * as ImageManipulator from 'expo-image-manipulator';
+import * as Asset from 'expo-asset';
+//import { Asset} from 'expo';
 import { createBottomTabNavigator } from 'react-navigation';
 import { baseUrl } from '../shared/baseUrl';
 
@@ -142,19 +145,33 @@ class RegisterTab extends Component {
   }
 
   getImageFromCamera = async () => {
-      const cameraPermission = await Permissions.askAsync(Permissions.CAMERA);
-      const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    const cameraPermission = await Permissions.askAsync(Permissions.CAMERA);
+    const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
 
-      if (cameraPermission.status === 'granted' && cameraRollPermission.status === 'granted') {
-          let capturedImage = await ImagePicker.launchCameraAsync({
-              allowsEditing: true,
-              aspect: [4, 3],
-          });
-          if (!capturedImage.cancelled) {
-              console.log(capturedImage);
-              this.setState({imageUrl: capturedImage.uri });
-          }
-      }
+    if (cameraPermission.status === 'granted' && cameraRollPermission.status === 'granted') {
+        let capturedImage = await ImagePicker.launchCameraAsync({
+            allowsEditing: true,
+            aspect: [4, 3],
+        });
+        if (!capturedImage.cancelled) {
+            console.log(capturedImage);
+            this.processImage(capturedImage.uri);
+        }
+    }
+
+  }
+
+  processImage = async (imageUri) => {
+    
+      let processedImage = await ImageManipulator.manipulateAsync(
+          imageUri, 
+          [
+              {resize: {width: 400}}
+          ],
+          {format: 'png'}
+      );
+      console.log(processedImage);
+      this.setState({imageUrl: processedImage.uri });
 
   }
   
